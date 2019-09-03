e07b71d6d4c9aa98c4789c5ce9be15579e2cdf6e
Thai Nguyen <discoverylight@yahoo.com>
2019-09-03 08:40:20 -0700

    finalized project

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require("express")
-const morgan = require("morgan")
-const bodyParser = require("body-parser")
+const express = require("express");
+const morgan = require("morgan");
+//const bodyParser = require("body-parser")
 const usersRouter = require("./routes/users");
 const app = express();
 
-
 app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", usersRouter);
 
-
 app.listen(5000, () => {
-    console.log("--server is listening on port 5000");
-})
+  console.log("--server is listening on port 5000");
+});
